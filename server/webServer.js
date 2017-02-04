6e8c111fea59221fbeeafda41d9888beec0a986c
AJTootell <up770605@myport.ac.uk>
2017-02-04 13:25:49 +0000

    near working db

--- server/webServer.js
+++ server/webServer.js
@@ -3,7 +3,8 @@ debugging = false,	//run with 'node artifact/server on' to see debugging message
 express = require('express'),
 app = express(),
 util =  require('./util.js'),
-mysql = require('mysql');
+mysql = require('mysql'),
+config = require('./config.json');
 //check if there is an additional argument, if it is on the console.log debugs
 if(process.argv.length == 3){
   util.debugable(process.argv[2])
@@ -12,7 +13,30 @@ if(process.argv.length == 3){
 app.use('/', express.static('webPages', { extensions: ['html'] }));
 
 
-app.get('/testDB', function(req, res) {
+app.get('/', function(req, res) {
+  var results = [];
+
+  sqlCon = mysql.createConnection(config.mysql)
+
+  sqlCon.on('error',function(err) {
+    console.error(err);
+    sqlCon.end();
+  });
+
+  sqlCon.query('select * from widget', function (err, data) {
+    if(err) {
+      cb(err);
+      console.error('failed to get all dis stuff', err);
+      sql.end();
+      return;
+    } else{
+      data.forEach(function(row){
+        results.push(row);
+      });
+      res.render('index', { list: results });
+      sql.end();
+    }
+  });
 
 });
 
